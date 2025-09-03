aadd1c33e7618a595652ac4c4473bc5a765622de
Samuel Scheit <github@samuelscheit.com>
2025-09-03 15:05:27 +0200

    App.tsx aktualisieren

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -21,7 +21,7 @@ const timeStrings: string[] = [
 	// "2025-08-30T23:25:16.459Z",
 	// "2025-08-30T20:26:19.217Z",
 	// "2025-08-30T17:25:07.835Z",
-"2025-08-25T21-47-23.259Z",
+"2025-08-25T21:47:23.259Z",
 	"2025-08-09T20:01:14.231Z",
 	// add further timestamps here
 ];
@@ -144,7 +144,7 @@ return;
 			? `https://proxy293.flam3rboy.workers.dev/https://backend.wplace.live/files/s0/tiles/{x}/{y}.png`
 			: `https://raw.githubusercontent.com/samuelscheit/wplace-archive/tiles/world-${currentSlug}/{z}/{x}/{y}.png`;
 
-if (currentTime.getTime() === new Date("2025-08-25T21-47-23.259Z").getTime()) {
+if (currentTime.getTime() === new Date("2025-08-25T21:47:23.259Z").getTime()) {
 timeTilesUrl = "/tiles/world-2025-08-25T21-47-23.259Z/{z}/{x}/{y}.png"
 
 }
