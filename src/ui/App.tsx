a04d0473de5b8dd8326241ec2f87d03218019192
samuelscheit <github@samuelscheit.com>
2025-10-12 01:32:28 +0200

    x

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -35,32 +35,20 @@ const timeStrings: string[] = [
 ];
 const defaultTimes = timeStrings.map((s) => new Date(s));
 
-interface DonationMethod {
-	label: string;
-	href: string;
-}
-
-const donationMethods: DonationMethod[] = [
-	{
-		label: "Patreon",
-		href: "https://www.patreon.com/samuelscheit",
-	},
-	{
-		label: "GitHub",
-		href: "https://github.com/sponsors/samuelscheit",
-	},
-	{
-		label: "PayPal",
-		href: "https://www.paypal.me/samuelscheit?locale.x=de_DE",
-	},
-];
-
-function DonateButton(props: AnchorHTMLAttributes<any>) {
+function DonateButton(props: AnchorHTMLAttributes<any> & { method?: string }) {
 	return (
 		<a
 			{...props}
 			target="_blank"
 			rel="noreferrer"
+			onClick={() => {
+				// @ts-ignore
+				globalThis?.plausible?.("donate_button", {
+					props: {
+						method: props.method,
+					},
+				});
+			}}
 			className={
 				"inline-flex items-center justify-center gap-2 self-start rounded px-3 py-1.5 text-sm font-medium text-neutral-50 shadow " +
 				props.className
@@ -367,6 +355,9 @@ function App() {
 	};
 
 	const openAbout = useCallback(() => {
+		// @ts-ignore
+		globalThis?.plausible?.("about");
+
 		const lang = globalThis.navigator?.language?.split("-")[0] || "en";
 		// @ts-ignore
 		globalThis.googleTranslateElementInit = () => {
@@ -393,7 +384,12 @@ function App() {
 		setIsAboutOpen(true);
 	}, []);
 	const closeAbout = useCallback(() => setIsAboutOpen(false), []);
-	const openDonate = useCallback(() => setIsDonateOpen(true), []);
+	const openDonate = useCallback(() => {
+		// @ts-ignore
+		globalThis?.plausible?.("donate");
+
+		setIsDonateOpen(true);
+	}, []);
 	const closeDonate = useCallback(() => setIsDonateOpen(false), []);
 
 	useEffect(() => {
@@ -602,8 +598,7 @@ function App() {
 							<p>Hi I'm Samuel 👋</p>
 							<p>
 								I created the wplace archive to <span className="bold">preserve</span> the entire wplace map{" "}
-								<span className="bold">history</span>{" "}
-								<span className="bold">without zoom limits</span>.
+								<span className="bold">history</span> <span className="bold">without zoom limits</span>.
 							</p>
 
 							<div>
@@ -614,17 +609,19 @@ function App() {
 							</div>
 
 							<div>
-								I'm currently paying <span className="codeblock">$20</span> a month to keep this project online with the help of{" "}
-								<span className="codeblock">1</span> donater.
+								I'm currently paying <span className="codeblock">$20</span> a month to keep this project online with the
+								help of <span className="codeblock">1</span> donater.
 							</div>
 
 							<div>
-								If you like the archive and would like to help to keep it online, please consider donating. It would help me a lot to cover the costs. Thank you very much :)
+								If you like the archive and would like to help to keep it online, please consider donating. It would help me
+								a lot to cover the costs. Thank you very much :)
 							</div>
 
 							<div>
-								Also <span className="bold">huge thanks</span> to the <span className="bold">supporters</span> (Nicolas Rodriguez) who make
-								preserving wplace possible and thank you to{" "}
+								Also <span className="bold">huge thanks</span> to the <span className="bold">supporters</span> (
+								<span className="bg-gray-500/10 ">Nicolas Rodriguez</span>) who make preserving wplace possible and thank
+								you to{" "}
 								<a
 									href="https://github.com/murolem/wplace-archives/"
 									target="_blank"
@@ -744,25 +741,7 @@ function App() {
 						</div>
 
 						<div className="flex flex-row gap-2 text-white flex-wrap items-stretch">
-							<DonateButton href="https://www.patreon.com/samuelscheit" style={{ backgroundColor: "#F96854" }}>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
-									<path
-										fill="currentColor"
-										d="M490 153.8c-.1-65.4-51-119-110.7-138.3-74.2-24-172-20.5-242.9 12.9-85.8 40.5-112.8 129.3-113.8 217.8-.8 72.8 6.4 264.4 114.6 265.8 80.3 1 92.3-102.5 129.5-152.3 26.4-35.5 60.5-45.5 102.4-55.9 72-17.8 121.1-74.7 121-150l-.1 0z"
-									/>
-								</svg>
-								Patreon
-							</DonateButton>
-							<DonateButton href="https://github.com/sponsors/samuelscheit" style={{ backgroundColor: "#000" }}>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
-									<path
-										fill="currentColor"
-										d="M173.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM252.8 8c-138.7 0-244.8 105.3-244.8 244 0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1 100-33.2 167.8-128.1 167.8-239 0-138.7-112.5-244-251.2-244zM105.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9s4.3 3.3 5.6 2.3c1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-									/>
-								</svg>
-								GitHub
-							</DonateButton>
-							<DonateButton href="https://www.paypal.me/samuelscheit" style={{ backgroundColor: "#0070e0" }}>
+							<DonateButton href="https://www.paypal.me/samuelscheit" style={{ backgroundColor: "#0070e0" }} method="paypal">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
 									<path
 										fill="currentColor"
@@ -771,7 +750,11 @@ function App() {
 								</svg>
 								PayPal
 							</DonateButton>
-							<DonateButton href="https://buymeacoffee.com/samuelscheit" style={{ backgroundColor: "#52a447" }}>
+							<DonateButton
+								href="https://buymeacoffee.com/samuelscheit"
+								style={{ backgroundColor: "#52a447" }}
+								method="buymeacoffee"
+							>
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
 									<path
 										fill="white"
@@ -780,7 +763,33 @@ function App() {
 								</svg>
 								Credit Card
 							</DonateButton>
-							<DonateButton href="https://qr.alipay.com/fkx175153zjoblrjcwh4ode" style={{ backgroundColor: "#0e9dec" }}>
+							<DonateButton
+								href="https://www.patreon.com/samuelscheit"
+								style={{ backgroundColor: "#F96854" }}
+								method="patreon"
+							>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
+									<path
+										fill="currentColor"
+										d="M490 153.8c-.1-65.4-51-119-110.7-138.3-74.2-24-172-20.5-242.9 12.9-85.8 40.5-112.8 129.3-113.8 217.8-.8 72.8 6.4 264.4 114.6 265.8 80.3 1 92.3-102.5 129.5-152.3 26.4-35.5 60.5-45.5 102.4-55.9 72-17.8 121.1-74.7 121-150l-.1 0z"
+									/>
+								</svg>
+								Patreon
+							</DonateButton>
+							<DonateButton
+								href="https://github.com/sponsors/samuelscheit"
+								style={{ backgroundColor: "#000" }}
+								method="github"
+							>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="size-4">
+									<path
+										fill="currentColor"
+										d="M173.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM252.8 8c-138.7 0-244.8 105.3-244.8 244 0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1 100-33.2 167.8-128.1 167.8-239 0-138.7-112.5-244-251.2-244zM105.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9s4.3 3.3 5.6 2.3c1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+									/>
+								</svg>
+								GitHub
+							</DonateButton>
+							<DonateButton href="https://qr.alipay.com/fkx175153zjoblrjcwh4ode" style={{ backgroundColor: "#0e9dec" }} method="alipay">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="size-4">
 									<path
 										fill="currentColor"
@@ -792,6 +801,7 @@ function App() {
 							<DonateButton
 								href="https://share.weropay.eu/p/1/c/TEtPuXbCOj"
 								style={{ backgroundColor: "#0e9dec", height: "32px" }}
+								method="wero"
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
